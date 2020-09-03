d52c5a65e1aacd3ab971a0cbb3fa00cc88e1437f
Russ <rperry1886@gmail.com>
2020-09-03 19:24:00 -0400

    Add drag and drop function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -20,6 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -63,6 +65,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
   // Append
   columnEl.appendChild(listEl);
 }
@@ -94,6 +98,35 @@ function updateDOM() {
     createItemEl(onHoldList, 0, onHoldItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+}
+
+// When Item starts dragging
+function drag(e) {
+  draggedItem = e.target;
+  console.log(draggedItem);
+}
+
+// Coulmn allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// When the item inters the column area
+function dragEnter(column) {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+// Droppign Item in column
+function drop(e) {
+  e.preventDefault();
+  // Remove background color padding
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  // Add item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // On Load
